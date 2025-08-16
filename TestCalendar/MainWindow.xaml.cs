e018c99b3278d2086b570ebbc8639a21f160caaf
jbautistam <jbautistam@gmail.com>
2025-08-16 15:15:24 +0200

    Event

--- TestCalendar/MainWindow.xaml.cs
+++ TestCalendar/MainWindow.xaml.cs
@@ -1,29 +1,19 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace TestCalendar
+namespace TestCalendar;
+
+/// <summary>
+///		Ventana principal
+/// </summary>
+public partial class MainWindow : Window
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public MainWindow()
 	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+		InitializeComponent();
+	}
 
-		private void DayView_AddAppointment(object sender, RoutedEventArgs e)
-		{
+	private void DayView_AddAppointment(object sender, RoutedEventArgs e)
+	{
 
-		}
 	}
 }
